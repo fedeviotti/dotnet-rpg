0c0bcc9e61629e8fc3632c22900deff4495cc3eb
Federico Viotti <fedeviotti@gmail.com>
2023-05-13 23:41:44 +0200

    feat(character-api): add automapper

--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Models;
 
@@ -5,24 +6,33 @@ namespace dotnet_rpg.Services.CharacterService;
 
 public class CharacterService : ICharacterService
 {
+    private readonly IMapper _mapper;
+
     private static readonly List<Character> Characters = new List<Character>
     {
         new Character(),
         new Character { Id = 1, Name = "Sam" }
     };
+
+    public CharacterService(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
     
     public async Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter)
     {
         var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-        Characters.Add(newCharacter);
-        serviceResponse.Data = Characters;
+        var character = _mapper.Map<Character>(newCharacter);
+        character.Id = Characters.Max(c => c.Id) + 1;
+        Characters.Add(character);
+        serviceResponse.Data = Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
         return serviceResponse;
     }
     
     public async Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters()
     {
         var serviceResponse = new ServiceResponse<List<GetCharacterDto>>();
-        serviceResponse.Data = Characters;
+        serviceResponse.Data = Characters.Select(c => _mapper.Map<GetCharacterDto>(c)).ToList();
         return serviceResponse;
     }
 
@@ -30,7 +40,7 @@ public class CharacterService : ICharacterService
     {
         var serviceResponse = new ServiceResponse<GetCharacterDto>();
         var character = Characters.FirstOrDefault(c => c.Id == id);
-        serviceResponse.Data = character;
+        serviceResponse.Data = _mapper.Map<GetCharacterDto>(character);
         return serviceResponse;
     }
 }
