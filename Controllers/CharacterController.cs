56856eca6e8c8f6c47b22afdb32653507fb2c289
Federico Viotti <fedeviotti@gmail.com>
2023-05-13 11:35:18 +0200

    feat(character-api): add models and get endpoints

---
+++ Controllers/CharacterController.cs
@@ -0,0 +1,28 @@
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CharacterController : ControllerBase
+{
+    private static List<Character> characters = new List<Character>
+    {
+        new Character(),
+        new Character { Id = 1, Name = "Sam" }
+    };
+    
+    [HttpGet("GetAll")]
+    public ActionResult<List<Character>> Get()
+    {
+        return Ok(characters);
+    }
+    
+    [HttpGet("{id}")]
+    public ActionResult<Character> GetSingle(int id)
+    {
+        return Ok(characters.FirstOrDefault(c => c.Id == id));
+    }
+    
+}
