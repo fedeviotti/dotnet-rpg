c0384adc20fab6c0ab7023212a648f15d3e27cba
Federico Viotti <fedeviotti@gmail.com>
2023-05-29 23:57:23 +0200

    feat: add auth controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+using dotnet_rpg.Data;
+using dotnet_rpg.Dtos.User;
+using dotnet_rpg.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_rpg.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : ControllerBase
+{
+    private readonly IAuthRepository _authRepository;
+
+    public AuthController(IAuthRepository authRepository)
+    {
+        _authRepository = authRepository;
+    }
+
+    [HttpPost("Register")]
+    public async Task<ActionResult<ServiceResponse<int>>> Register(UserRegisterDto request)
+    {
+        var user = new User
+        {
+            Username = request.Username
+        };
+        var response = await _authRepository.Register(user, request.Password);
+        if (!response.Success)
+        {
+            return BadRequest(response);
+        }
+        return Ok(response);
+    }
+}
