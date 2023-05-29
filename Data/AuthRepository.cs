127fa8af29d7b6a27162ed1bb1d8001316132697
Federico Viotti <fedeviotti@gmail.com>
2023-05-29 23:35:48 +0200

    chore(auth): create AuthRepository.cs

---
+++ Data/AuthRepository.cs
@@ -0,0 +1,55 @@
+using System.Security.Cryptography;
+using System.Text;
+using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_rpg.Data;
+
+public class AuthRepository : IAuthRepository
+{
+    private readonly DataContext _context;
+
+    public AuthRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<ServiceResponse<int>> Register(User user, string password)
+    {
+        var response = new ServiceResponse<int>();
+        if (await UserExists(user.Username))
+        {
+            response.Success = false;
+            response.Message = "User already exists.";
+            return response;
+        }
+        
+        CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+        
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+        
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        response.Data = user.Id;
+        return response;
+    }
+
+    public Task<ServiceResponse<string>> Login(string username, string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UserExists(string username)
+    {
+        return await _context.Users.AnyAsync((u) =>
+            string.Equals(u.Username.ToLower(), username.ToLower(), StringComparison.Ordinal));
+    }
+    
+    private static void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+    {
+        using var hmac = new HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+    }
+}
