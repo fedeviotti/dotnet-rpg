d68cc9a2f5e0b459055f5d045e5e04efacefec7a
Federico Viotti <fedeviotti@gmail.com>
2023-06-17 16:11:09 +0200

    feat(auth): add auth to character controller

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,10 +1,12 @@
 using dotnet_rpg.Dtos.Character;
 using dotnet_rpg.Models;
 using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rpg.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/[controller]")]
 public class CharacterController : ControllerBase
